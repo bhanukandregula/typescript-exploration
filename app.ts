70d3f80cd334032055e96a73f4e1bc86c68bfaaf
Bhanu Prakash Kandregula <bhanuprakashkandregula@Bhanus-MBP.lan>
2020-12-09 08:32:48 -0500

    second comit

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
 console.log("This is Bhanu, from Type Script exploration.");
+
+function add(n1: number, n2: number){
+    console.log(typeof number1);
+    return n1 + n2;
+}
+
+const number1 = 23;
+const number2 = 34.8;
+const result = add(number1, number2);
+
+console.log("Sum is: ", result);
